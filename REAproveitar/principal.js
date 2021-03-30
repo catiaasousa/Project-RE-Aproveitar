824f0ff45a24c1ca397cd145314e0ff7540ac627
catiaasousa <catiasousa27@gmail.com>
2021-03-30 22:50:57 +0100

    Renomeado o ficheiro "principal.md" para "principal.html" Criada a pasta "images"para construção de background. Criados ficheiro principal.js para funcionamento da página.

---
+++ REAproveitar/principal.js
@@ -0,0 +1,4 @@
+function validadiahorario(){
+    document.getElementById("mapa").innerHTML = "Mapas ainda não disponiveis";    
+    //codigo que será efetuado para devolver o horario na pagina principal
+}

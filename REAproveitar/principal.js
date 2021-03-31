9d59c932d777b2f7ce82060467ae4433b279490a
catiaasousa <catiasousa27@gmail.com>
2021-04-01 00:09:01 +0100

    Adicionado ficheiro principal.css e respectivas alterações nos ficheiros

--- REAproveitar/principal.js
+++ REAproveitar/principal.js
@@ -1,4 +1,5 @@
 function validadiahorario(){
-    document.getElementById("mapa").innerHTML = "Mapas ainda não disponiveis";    
+    //document.getElementById("mapa").innerHTML = "Mapas ainda não disponiveis";    
     //codigo que será efetuado para devolver o horario na pagina principal
+    document.getElementById("mapa").style.visibility = "visible";
 }
